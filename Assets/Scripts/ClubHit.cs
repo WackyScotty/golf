550dca8889c930357163dc803eddbf4d33c5397e
Davis2600 <dwojnovich@gmail.com>
2022-09-25 15:34:07 -0400

    testing, some script updates

--- Assets/Scripts/ClubHit.cs
+++ Assets/Scripts/ClubHit.cs
@@ -1,25 +1,43 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ClubHit : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Idea, we want to be averaging the golf hits over a set amount of
+    // For now the last half of a second
+    // fixedUpdate updates every 0.02 seconds
+    private Vector3[] _directionArrays;
+    private int _currentDirIndex;
+    
     private Vector3 _hitDirection;
     private Vector3 _oldPosition;
-    void Start()
+    private Vector3 _arraySum; // declared here to avoid the cost of declaration at collision
+    void Start() 
     {
-        _hitDirection = new Vector3(0, 0, 0);
-        _oldPosition = this.gameObject.transform.position;
+        _directionArrays = new Vector3[50];
+        for(var i = 0; i < _directionArrays.Length; i++)
+        {
+            _directionArrays[i] = Vector3.zero;
+        }
+
+        _currentDirIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    private void FixedUpdate()
+    {
         Vector3 currentPosition = this.gameObject.transform.position;
-        _hitDirection = (currentPosition - _oldPosition).normalized;
-        _oldPosition = currentPosition;
+        _directionArrays[_currentDirIndex] = currentPosition - _oldPosition;
+        _currentDirIndex = (_currentDirIndex + 1) % 50;
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -28,9 +46,16 @@ public class ClubHit : MonoBehaviour
         if (collision.gameObject.CompareTag("ball"))
         {
             Debug.Log("Ball Hit!!");
-            Debug.DrawRay(collision.gameObject.transform.position, _hitDirection, Color.red, 10.0f, true);
-            // TODO compute this vector
-            collision.gameObject.GetComponent<Rigidbody>().AddForce(3 * _hitDirection, ForceMode.Impulse);
+            Debug.DrawRay(collision.gameObject.transform.position, _hitDirection, Color.red, 10000.0f, false);
+            // take the average of our direction array
+            Vector3 _arraysum = Vector3.zero;
+            for (var i = 0; i < _directionArrays.Length; i++)
+            {
+                _arraysum += _directionArrays[i];
+            }
+
+            _arraysum /= _directionArrays.Length;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(5 * _arraysum, ForceMode.Impulse);
         }
     }
 }
